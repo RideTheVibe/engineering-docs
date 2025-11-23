4ea1c840c52b84a686a0402b093bcce09cbff259
Chitlange Sahas <chitlangesahas@gmail.com>
2025-11-23 09:03:21 -0700

    route to main page when going home

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: 'https://ridethevibe.io',
+                permanent: false,
+            },
+        ]
+    },
 };
 
 export default withMDX(config);
